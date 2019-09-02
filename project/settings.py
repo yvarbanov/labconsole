441609bbba4069125d2cfb45953856fd3c67f67f
Alberto Gonzalez <alberto.gonzalez@redhat.com>
2019-09-02 17:12:16 -0500

    labconsole

--- project/settings.py
+++ project/settings.py
@@ -36,14 +36,10 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
-    'welcome',
+    'labconsole',
 ]
 
 MIDDLEWARE = [
