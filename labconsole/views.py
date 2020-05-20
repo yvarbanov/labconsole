d5bbb85d5094824d3c0acce11c443ed4f327670b
agonzalezrh <alberto.gonzalez@redhat.com>
2020-05-20 16:03:46 +0200

    Console improvement

--- labconsole/views.py
+++ labconsole/views.py
@@ -68,9 +68,12 @@ def stop_vm(request, server):
 def restart_vm(request, server):
     conn = _connect()
     conn.compute.reboot_server(server, reboot_type="HARD")
-    resp = JsonResponse({'success': 1})
-    resp['Access-Control-Allow-Origin'] = '*'
-    return resp  
+    response = JsonResponse({'success': 1})
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
 
 def rebuild_vm(request, server, name, image):
     conn = _connect()
