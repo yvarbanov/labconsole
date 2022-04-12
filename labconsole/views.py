70674e43eb1d6774bc26bccefde8d1ea94969b67
yvarbanov <yvarbev@redhat.com>
2022-04-12 14:49:52 +0200

    adding support for clouds.yaml and multi cluster OPS

--- labconsole/views.py
+++ labconsole/views.py
@@ -1,72 +1,76 @@
-import os
 from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import redirect
-
-
+from django import template
 from . import database
 from .models import LabConsole 
-
 import openstack
 from openstack.compute import compute_service
+import os
 import json
+import yaml
 
-def _connect():
-    server = os.getenv('OS_AUTH_URL')
-    project = os.getenv('OS_PROJECT_NAME')
-    username = os.getenv('OS_USERNAME')
-    password = os.getenv('OS_PASSWORD')
-    return openstack.connection.Connection(
-                auth=dict(
-                auth_url=server,
-                project_name=project,
-                username=username,
-                password=password,
-                project_domain_name="Default",
-                user_domain_name="Default"),
-                identity_api_version=3,
-                region_name="regionOne",
-            )
 
+def _connect(cloud):
+    if os.getenv('OS_CLIENT_CONFIG_FILE'):
+        return openstack.connection.Connection(cloud=cloud)
+    else:
+        server = os.getenv('OS_AUTH_URL')
+        project = os.getenv('OS_PROJECT_NAME')
+        username = os.getenv('OS_USERNAME')
+        password = os.getenv('OS_PASSWORD')
+        return openstack.connection.Connection(
+                    auth=dict(
+                    auth_url=server,
+                    project_name=project,
+                    username=username,
+                    password=password,
+                    project_domain_name="Default",
+                    user_domain_name="Default"),
+                    identity_api_version=3,
+                    region_name="regionOne",
+                )
+
+def _vm_to_project(conn, user, projects):
+    for project in conn.identity.projects():
+        for tag in project.tags:
+            if user in tag:
+                projects[project.name] = []
+                for vm in conn.compute.servers(all_tenants=1,project_id=project.id):
+                    projects[project.name].append(vm)
+    return projects
 
 def index(request):
-    conn = _connect()
-    vms = conn.compute.servers()
     user = request.META.get("HTTP_X_FORWARDED_USER") or os.getenv('TEST_USER')
     if not user:
        user = ""
-    search = {"tags":"student=" + user}
     projects = {}
-    for project in conn.identity.projects(**search):
-       projects[project.name] = []
-       for vm in conn.compute.servers(all_tenants=1,project_id=project.id):
-           projects[project.name].append(vm)
-
+    if os.getenv('OS_CLIENT_CONFIG_FILE'):
+        with open(os.getenv('OS_CLIENT_CONFIG_FILE')) as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)
+        clouds = [*data['clouds'].keys()]
+        for cloud in clouds:
+            conn = _connect(cloud)
+            projects = _vm_to_project(conn, user, projects)
+    else:
+        conn = _connect()
+        projects = _vm_to_project(conn, user, projects)
     return render(request, 'index.html', {'projects': projects, 'user': user })
-    #return render(request, 'welcome/index.html', {
-    #    'hostname': hostname,
-    #    'database': database.info(),
-    #    'count': PageView.objects.count()
-    #})
 
-def get_vm(self, name):
-    """Get a VM by name."""
-    vms = self._client.compute.servers()
-
-def start_vm(request, server):
-    conn = _connect()
+def start_vm(request, server, cloud):
+    conn = _connect(cloud)
     conn.compute.start_server(server)
     return HttpResponse(1)
 
-def stop_vm(request, server):
-    conn = _connect()
+def stop_vm(request, server, cloud):
+    conn = _connect(cloud)
     conn.compute.stop_server(server)
     return HttpResponse(1)
 
-def restart_vm(request, server):
-    conn = _connect()
+def restart_vm(request, server, cloud):
+    conn = _connect(cloud)
     conn.compute.reboot_server(server, reboot_type="HARD")
     response = JsonResponse({'success': 1})
     response["Access-Control-Allow-Origin"] = "*"
@@ -75,39 +79,33 @@ def restart_vm(request, server):
     response["Access-Control-Allow-Headers"] = "*"
     return response
 
-def rebuild_vm(request, server, name, image):
-    conn = _connect()
-    #vm = conn.compute.get_server(server)
+def rebuild_vm(request, server, name, image, cloud):
+    conn = _connect(cloud)
     attrs = {"image": image}
     conn.compute.rebuild_server(server, name, "1234", **attrs)
     return HttpResponse(1)
 
-def rescue_vm(request, server, image):
-    conn = _connect()
+def rescue_vm(request, server, image, cloud):
+    conn = _connect(cloud)
     image = conn.image.find_image(image)
     conn.compute.rescue_server(server, admin_pass="", image_ref=image.id)
     return HttpResponse(1)
 
-def unrescue_vm(request, server):
-    conn = _connect()
+def unrescue_vm(request, server, cloud):
+    conn = _connect(cloud)
     conn.compute.unrescue_server(server)
     return HttpResponse(1)
 
-
-
-def console(request, server):
-    conn = _connect()
+def console(request, server, cloud):
+    conn = _connect(cloud)
     body = {'os-getVNCConsole': {'type': 'novnc'}}
     headers = {'Accept': ''}
     resp = conn.session.post( '/servers/{server_id}/action'.format(server_id=server),json=body, headers=headers, endpoint_filter={'service_type': 'compute'})
     content = json.loads(resp.content)
     vm = conn.compute.get_server(server)
-    #response = redirect(content["console"]["url"]) 
     response = render(request, 'console.html', {'console': content["console"]["url"].replace("auto", "novello"), 'vm': vm})
     response["Access-Control-Allow-Origin"] = "*"
     return response
 
-
 def health(request):
     return HttpResponse(1)
-
